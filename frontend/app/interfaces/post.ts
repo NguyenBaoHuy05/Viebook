e53b74584bff491c146523967fead3edb5153323
khangdepzaivodich <phantuankhanghcmue@gmail.com>
2025-04-07 00:16:30 +0700

    some changes

---
+++ frontend/app/interfaces/post.ts
@@ -0,0 +1,10 @@
+export default interface iPost {
+  name: string;
+  logo: string;
+  title: string;
+  content: string;
+  commentCount: number;
+  reactCount: number;
+  shareCount: number;
+  date: string;
+}
